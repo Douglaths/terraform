2c1653819b3db6db6e11791a958fddd0a1c0da8a
Douglaths <douglathsmelo@gmail.com>
2024-10-12 10:44:26 -0500

    EC2

--- vpc.tf
+++ vpc.tf
@@ -21,7 +21,7 @@ resource "aws_internet_gateway" "igw" {
 }
 
 # Configuramos la primera red pública en otra zona de disponibilidad
-resource "aws_subnet" "First_public_subnet" {
+resource "aws_subnet" "first_public_subnet" {
   vpc_id                  = aws_vpc.vpc_cloud_2.id
   cidr_block              = "30.0.1.0/24"
   availability_zone       = "us-east-1a"
@@ -100,4 +100,31 @@ resource "aws_route_table_association" "private_association_1" {
 resource "aws_route_table_association" "private_association_2" {
   subnet_id      = aws_subnet.second_private_subnet.id
   route_table_id = aws_route_table.private_route_table.id
+}
+
+
+# Creamos el grupo de seguridad para las instancias EC2
+resource "aws_security_group" "my_security_group" {
+  vpc_id = aws_vpc.vpc_cloud_2.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Name = "MySecurityGroup"
+  }
 }
